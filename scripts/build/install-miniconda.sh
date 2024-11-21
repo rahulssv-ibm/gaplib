8602c2d5955423b46676d38619340c44999a5d1b
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2024-11-21 14:09:29 +0530

    update

--- scripts/build/install-miniconda.sh
+++ scripts/build/install-miniconda.sh
@@ -7,13 +7,26 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/etc-environment.sh
 
-# Install Miniconda
-curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh \
-    && chmod +x miniconda.sh \
-    && ./miniconda.sh -b -p /usr/share/miniconda \
-    && rm miniconda.sh
+if [ "$ARCH" = "ppc64le" ] ; then 
+    # Install Miniconda
+    curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-ppc64le.sh -o miniconda.sh \
+        && chmod +x miniconda.sh \
+        && ./miniconda.sh -b -p /usr/share/miniconda \
+        && rm miniconda.sh
 
-CONDA=/usr/share/miniconda
-set_etc_environment_variable "CONDA" "${CONDA}"
+    CONDA=/usr/share/miniconda
+    set_etc_environment_variable "CONDA" "${CONDA}"
 
-ln -s $CONDA/bin/conda /usr/bin/conda
+    ln -s $CONDA/bin/conda /usr/bin/conda
+else
+    # Install Miniconda
+    curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh \
+        && chmod +x miniconda.sh \
+        && ./miniconda.sh -b -p /usr/share/miniconda \
+        && rm miniconda.sh
+
+    CONDA=/usr/share/miniconda
+    set_etc_environment_variable "CONDA" "${CONDA}"
+
+    ln -s $CONDA/bin/conda /usr/bin/conda
+fi
