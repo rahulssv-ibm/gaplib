b71ab6a8cd6bf76ad7c20ee8cb51d36f8c051c3c
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2024-12-12 20:26:02 +0530

    update

--- build-files/install-packages.sh
+++ build-files/install-packages.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+header() {
+    TS=`date +"%Y-%m-%dT%H:%M:%S%:z"`
+    echo "${TS} +--------------------------------------------+"
+    echo "${TS} | $*"
+    echo "${TS} +--------------------------------------------+"
+    echo
+}
+
+msg() {
+    echo `date +"%Y-%m-%dT%H:%M:%S%:z"` $*
+}
 
 # Check if the file name is provided
 if [ -z "$1" ]; then
@@ -13,17 +24,19 @@ if [ ! -f "$PACKAGE_LIST_FILE" ]; then
     echo "File $PACKAGE_LIST_FILE not found!"
     exit 1
 fi
-
+header "Installing additional packages"
 # Update the package list
-echo "Updating package list..."
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update -y
+msg "Updating package list..."
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y >/dev/null
 
 # installing docker
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
+do
+    sudo apt-get remove $pkg >/dev/null
+done
 
 # Add Docker's official GPG key:
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install ca-certificates curl -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install ca-certificates curl -y >/dev/null
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -38,12 +51,24 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Read the file line by line and install each package
+OK=`true`
 while IFS= read -r package || [ -n "$package" ]; do
     if [[ ! -z "$package" && "$package" != \#* ]]; then
         echo "Installing $package..."
-        sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y "$package"
+        sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y "$package" >/dev/null
+        if [ $? -ne 0 ]; then
+            OK=`false`
+        fi
         sudo dpkg --configure -a
     fi
 done < "$PACKAGE_LIST_FILE"
 
-echo "All packages from $PACKAGE_LIST_FILE have been installed."
+if [ ${OK} ]; then
+    msg "All packages from $PACKAGE_LIST_FILE have been installed."
+else
+    msg "NOT all packages from $PACKAGE_LIST_FILE have been installed - check log."
+fi
+
+# Removing the packaging cache
+sudo rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
+sudo apt-get clean
