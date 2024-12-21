6d5a0abd8c621f5cb2382eb7dbc0673ce298504d
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2024-12-21 07:11:14 +0530

    update

--- images/ubuntu/scripts/test.sh
+++ images/ubuntu/scripts/test.sh
@@ -60,7 +60,7 @@ done
 
 # run_script "${path_root}/../scripts/build/install-docker.sh" "DOCKERHUB_PULL_IMAGES" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
 
-run_script "${path_root}/../scripts/build/install-pipx-packages.sh" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
+# run_script "${path_root}/../scripts/build/install-pipx-packages.sh" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
 
 run_script "${path_root}/../scripts/build/install-homebrew.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
 
