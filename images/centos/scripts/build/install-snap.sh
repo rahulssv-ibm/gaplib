d91f0fa90fa0ee9a7d4fa3d826d9a60b1464e7c0
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-01-01 20:33:23 +0530

    update

--- images/centos/scripts/build/install-snap.sh
+++ images/centos/scripts/build/install-snap.sh
@@ -4,7 +4,7 @@
 ##  Desc:  Install snapd
 ################################################################################
 source $HELPER_SCRIPTS/install.sh
-apt-get -y install snapd
+dnf -y install snapd
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 export PATH=/snap/bin/:$PATH
