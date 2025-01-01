25a45676c3a049381ce73518013c2605198ce3ed
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-01-01 12:59:12 +0530

    update

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,10 +3,9 @@
 # Function to ensure Docker is installed and available
 ensure_docker() {
     if ! command -v docker &> /dev/null; then
-        local HELPER_SCRIPTS=${SRCDIR}/../images/${HOST_OS_NAME}/scripts/helpers
-        local DOCKERHUB_PULL_IMAGES="NO"
+        local 
         echo "Docker is not installed. Attempting to install Docker..."
-        if sudo sh -c "${SRCDIR}/../images/${HOST_OS_NAME}/scripts/build/install-docker.sh ${DOCKERHUB_PULL_IMAGES} ${HELPER_SCRIPTS} ${ARCH}"; then
+        if sudo sh -c "HELPER_SCRIPTS='${SRCDIR}/../images/${HOST_OS_NAME}/scripts/helpers' DOCKERHUB_PULL_IMAGES='NO' ARCH=${ARCH} ${SRCDIR}/../images/${HOST_OS_NAME}/scripts/build/install-docker.sh"; then
             echo "Docker installed successfully."
         else
             echo "Failed to install Docker. Please check your system configuration." >&2
