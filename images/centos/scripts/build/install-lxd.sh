4abd90b5c64998d517c4ae69305c0ab546ddeb0e
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-01-02 19:39:29 +0530

    update

--- images/centos/scripts/build/install-lxd.sh
+++ images/centos/scripts/build/install-lxd.sh
@@ -6,6 +6,16 @@
 source $HELPER_SCRIPTS/install.sh
 
 sudo snap install lxd
-lxd init --auto
-# lxc storage set default volume.block.filesystem xfs
+echo "Waiting for LXD daemon to start..."
+sudo systemctl start snap.lxd.daemon
+sudo systemctl enable snap.lxd.daemon
+
+# Check if LXD command is now available
+if ! command -v lxd &> /dev/null; then
+  echo "LXD command still not found. Please check your snap installation."
+  exit 1
+else
+  lxd init --auto
+  echo "LXD is ready to use!"
+fi
 systemctl restart systemd-sysctl
