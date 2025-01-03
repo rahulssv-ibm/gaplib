81748e0cac163e86fa552fe96584001c1a3eae9a
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-01-03 16:19:00 +0530

    update

--- images/ubuntu/scripts/build/install-google-cloud-cli.sh
+++ images/ubuntu/scripts/build/install-google-cloud-cli.sh
@@ -3,17 +3,28 @@
 ##  File:  install-google-cloud-cli.sh
 ##  Desc:  Install the Google Cloud CLI
 ################################################################################
-REPO_URL="https://packages.cloud.google.com/apt"
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/install.sh
 
-# Install the Google Cloud CLI
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" > /etc/apt/sources.list.d/google-cloud-sdk.list
-wget -qO- https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor > /usr/share/keyrings/cloud.google.gpg
-apt-get update
-apt-get install google-cloud-cli
+if [[ "$ARCH" == "ppc64le" ]]; then 
+    # Placeholder for ppc64le-specific logic
+    echo "No actions defined for ppc64le architecture."
+elif [[ "$ARCH" == "s390x" ]]; then
+    # Placeholder for s390x-specific logic
+    echo "No actions defined for s390x architecture."
+else
+    REPO_URL="https://packages.cloud.google.com/apt"
 
-# remove apt
-rm /etc/apt/sources.list.d/google-cloud-sdk.list
-rm /usr/share/keyrings/cloud.google.gpg
+    # Install the Google Cloud CLI
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" > /etc/apt/sources.list.d/google-cloud-sdk.list
+    wget -qO- https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor > /usr/share/keyrings/cloud.google.gpg
+    apt-get update
+    apt-get install google-cloud-cli
 
-# add repo to the apt-sources.txt
-echo "google-cloud-sdk $REPO_URL" >> $HELPER_SCRIPTS/apt-sources.txt
+    # remove apt
+    rm /etc/apt/sources.list.d/google-cloud-sdk.list
+    rm /usr/share/keyrings/cloud.google.gpg
+
+    # add repo to the apt-sources.txt
+    echo "google-cloud-sdk $REPO_URL" >> $HELPER_SCRIPTS/apt-sources.txt
+fi
