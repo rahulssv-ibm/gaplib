8cd86ff01867a03e11bf067c76e0ac8fc3f98268
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-01-03 23:06:55 +0530

    test

--- scripts/helpers/setup_install.sh
+++ scripts/helpers/setup_install.sh
@@ -7,42 +7,42 @@ CURRENT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source ${CURRENT_DIR}/setup_vars.sh
 source ${CURRENT_DIR}/run_script.sh
 # Configure limits
-run_script "${INSTALLER_SCRIPT_FOLDER}/configure-limits.sh" 
+# run_script "${INSTALLER_SCRIPT_FOLDER}/configure-limits.sh" 
 
-# Configure image data
-run_script "${INSTALLER_SCRIPT_FOLDER}/configure-image-data.sh" "IMAGE_VERSION" "IMAGEDATA_FILE"
+# # Configure image data
+# run_script "${INSTALLER_SCRIPT_FOLDER}/configure-image-data.sh" "IMAGE_VERSION" "IMAGEDATA_FILE"
 
-# Configure environment
-run_script "${INSTALLER_SCRIPT_FOLDER}/configure-environment.sh" "IMAGE_OS" "IMAGE_VERSION" "HELPER_SCRIPTS"
+# # Configure environment
+# run_script "${INSTALLER_SCRIPT_FOLDER}/configure-environment.sh" "IMAGE_OS" "IMAGE_VERSION" "HELPER_SCRIPTS"
 
-if [[ "$IMAGE_OS" == *"ubuntu"* ]]; then
-    # Add apt wrapper to implement retries
-    run_script "${INSTALLER_SCRIPT_FOLDER}/configure-apt-mock.sh"
-    echo "Setting user ubuntu with sudo privileges"
+# if [[ "$IMAGE_OS" == *"ubuntu"* ]]; then
+#     # Add apt wrapper to implement retries
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/configure-apt-mock.sh"
+#     echo "Setting user ubuntu with sudo privileges"
 
-    # Install Configure apt
-    run_script "${INSTALLER_SCRIPT_FOLDER}/configure-apt.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS"
+#     # Install Configure apt
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/configure-apt.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS"
 
-    run_script "${INSTALLER_SCRIPT_FOLDER}/install-apt-vital.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/install-apt-vital.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
 
-    run_script "${INSTALLER_SCRIPT_FOLDER}/install-apt-common.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/install-apt-common.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
 
-    run_script "${INSTALLER_SCRIPT_FOLDER}/configure-dpkg.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
-elif [[ "$IMAGE_OS" == *"centos"* ]]; then
-    # Add apt wrapper to implement retries
-    run_script "${INSTALLER_SCRIPT_FOLDER}/configure-yum-mock.sh"
-    echo "Setting user ubuntu with sudo privileges"
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/configure-dpkg.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
+# elif [[ "$IMAGE_OS" == *"centos"* ]]; then
+#     # Add apt wrapper to implement retries
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/configure-yum-mock.sh"
+#     echo "Setting user ubuntu with sudo privileges"
 
-    # Install Configure apt
-    run_script "${INSTALLER_SCRIPT_FOLDER}/configure-dnf.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS"
+#     # Install Configure apt
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/configure-dnf.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS"
 
-    run_script "${INSTALLER_SCRIPT_FOLDER}/install-dnf-vital.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/install-dnf-vital.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
 
-    run_script "${INSTALLER_SCRIPT_FOLDER}/install-dnf-common.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/install-dnf-common.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER"
 
-    run_script "${INSTALLER_SCRIPT_FOLDER}/configure-dnfpkg.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER" 
+#     run_script "${INSTALLER_SCRIPT_FOLDER}/configure-dnfpkg.sh" "DEBIAN_FRONTEND" "HELPER_SCRIPTS" "INSTALLER_SCRIPT_FOLDER" 
 
-fi
+# fi
 
 # Initialize an empty array for script files
 SCRIPT_FILES=()
@@ -64,53 +64,53 @@ if [ "$SETUP" == "minimal" ]; then
 elif [ "$SETUP" == "complete" ]; then
     # List of scripts to be executed
     SCRIPT_FILES=(
-        "install-dotnetcore-sdk.sh"
-        "install-runner-package.sh"
-        "install-actions-cache.sh"
-        "install-azcopy.sh"
-        "install-azure-cli.sh"
-        "install-azure-devops-cli.sh"
-        "install-bicep.sh"
-        "install-aliyun-cli.sh"
-        "install-apache.sh"
-        "install-aws-tools.sh"
-        "install-clang.sh"
-        "install-swift.sh"
-        "install-cmake.sh"
-        "install-codeql-bundle.sh"
-        "install-container-tools.sh"
-        "install-firefox.sh"
-        "install-microsoft-edge.sh"
-        "install-gcc-compilers.sh"
-        "install-gfortran.sh"
-        "install-git.sh"
-        "install-git-lfs.sh"
-        "install-github-cli.sh"
-        "install-google-chrome.sh"
-        "install-google-cloud-cli.sh"
-        "install-haskell.sh"
-        "install-heroku.sh"
-        "install-java-tools.sh"
-        "install-kubernetes-tools.sh"
-        "install-oc-cli.sh"
-        "install-leiningen.sh"
-        "install-miniconda.sh"
-        "install-kotlin.sh"
-        "install-mysql.sh"
-        "install-mssql-tools.sh"
-        "install-sqlpackage.sh"
-        "install-nginx.sh"
-        "install-nvm.sh"
-        "install-nodejs.sh"
-        "install-bazel.sh"
-        "install-php.sh"
-        "install-postgresql.sh"
-        "install-pulumi.sh"
-        "install-ruby.sh"
-        "install-rlang.sh"
-        # "install-rust.sh"
-        "install-julia.sh"
-        "install-selenium.sh"
+        # "install-dotnetcore-sdk.sh"
+        # "install-runner-package.sh"
+        # "install-actions-cache.sh"
+        # "install-azcopy.sh"
+        # "install-azure-cli.sh"
+        # "install-azure-devops-cli.sh"
+        # "install-bicep.sh"
+        # "install-aliyun-cli.sh"
+        # "install-apache.sh"
+        # "install-aws-tools.sh"
+        # "install-clang.sh"
+        # "install-swift.sh"
+        # "install-cmake.sh"
+        # "install-codeql-bundle.sh"
+        # "install-container-tools.sh"
+        # "install-firefox.sh"
+        # "install-microsoft-edge.sh"
+        # "install-gcc-compilers.sh"
+        # "install-gfortran.sh"
+        # "install-git.sh"
+        # "install-git-lfs.sh"
+        # "install-github-cli.sh"
+        # "install-google-chrome.sh"
+        # "install-google-cloud-cli.sh"
+        # "install-haskell.sh"
+        # "install-heroku.sh"
+        # "install-java-tools.sh"
+        # "install-kubernetes-tools.sh"
+        # "install-oc-cli.sh"
+        # "install-leiningen.sh"
+        # "install-miniconda.sh"
+        # "install-kotlin.sh"
+        # "install-mysql.sh"
+        # "install-mssql-tools.sh"
+        # "install-sqlpackage.sh"
+        # "install-nginx.sh"
+        # "install-nvm.sh"
+        # "install-nodejs.sh"
+        # "install-bazel.sh"
+        # "install-php.sh"
+        # "install-postgresql.sh"
+        # "install-pulumi.sh"
+        # "install-ruby.sh"
+        # "install-rlang.sh"
+        # # "install-rust.sh"
+        # "install-julia.sh"
+        # "install-selenium.sh"
         "install-terraform.sh"
         "install-packer.sh"
         "install-vcpkg.sh"
