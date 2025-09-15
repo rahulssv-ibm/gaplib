70a48e548830143539906befdd51ecd96432b9b5
Anup Kodlekere <100858233+anup-kodlekere@users.noreply.github.com>
2025-09-15 17:37:28 +0530

    Update LXD installation to pin to LTS version and to hold autorefresh
    This commit disables autorefresh to the LXD snap to avoid any unplanned service disruptions caused due to snap refresh.

--- images/ubuntu/scripts/build/install-lxd.sh
+++ images/ubuntu/scripts/build/install-lxd.sh
@@ -5,15 +5,23 @@
 ################################################################################
 source $HELPER_SCRIPTS/install.sh
 
-# Install LXD using snap
-echo "Installing LXD using snap..."
-sudo snap install lxd
+# Install 5.21 LTS LXD version using snap
+echo "Installing LXD version 5.21 using snap..."
+sudo snap install lxd --channel="5.21/stable"
+
+echo "Printing LXD info..."
+lxc info
+
+echo "Checking list of refreshable snaps..."
+sudo snap refresh --list
 
 echo "Checking the status of snap.lxd.daemon..."
 ensure_service_is_active snap.lxd.daemon
+
+# Hold the autorefresh for LXD as it can cause unwanted service-disruptions 
+sudo snap refresh --hold lxd
 
 # Initialize LXD
 echo "Initializing LXD..."
 cat $INSTALLER_SCRIPT_FOLDER/lxd-preseed.yaml | sudo -i lxd init --preseed
 echo "LXD is ready to use!"
-
