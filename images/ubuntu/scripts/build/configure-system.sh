1454a04a9ca9c2a0ac0d293a3ccf80c7a416ca54
Rahul Vishwakarma <168823860+rahulssv-ibm@users.noreply.github.com>
2025-09-17 21:31:21 +0530

    fix: update image folder references in scripts for consistency

--- images/ubuntu/scripts/build/configure-system.sh
+++ images/ubuntu/scripts/build/configure-system.sh
@@ -6,7 +6,7 @@
 source $HELPER_SCRIPTS/etc-environment.sh
 source $HELPER_SCRIPTS/os.sh
 
-mv -f /var/tmp/imagegeneration/post-generation /opt
+mv -f "${IMAGE_FOLDER}/post-generation" /opt
 
 echo "chmod -R 777 /opt"
 chmod -R 777 /opt
--- scripts/helpers/setup_vars.sh
+++ scripts/helpers/setup_vars.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e  # Exit on any error
 
-toolset_file_name="toolset-$(echo "$2" | sed 's/\.//g').json"
-image_folder="/var/tmp/imagegeneration"
-helper_script_folder="${image_folder}/helpers"
-installer_script_folder="${image_folder}/installers"
-imagedata_file="${image_folder}/imagedata.json"
-
 # Default environment variable values
 ARCH=${ARCH:-$(uname -m)}
-HELPER_SCRIPTS="${helper_script_folder}"
-IMAGE_FOLDER="${image_folder}"
 IMAGE_OS=$1
 IMAGE_VERSION=$2
 WORKER_TYPE=${3:-""} # Default to "" if WORKER_TYPE is not set
 WORKER_CPU=${4:-""} # Default to "" if WORKER_CPU is not set
 SETUP=${5:-"minimal"} # Default to "minimal" if SETUP is not set
+
+toolset_file_name="toolset-$(echo "$2" | sed 's/\.//g').json"
+image_folder="/var/tmp/imagegeneration-${IMAGE_OS}-${IMAGE_VERSION}"
+helper_script_folder="${image_folder}/helpers"
+installer_script_folder="${image_folder}/installers"
+imagedata_file="${image_folder}/imagedata.json"
+
+HELPER_SCRIPTS="${helper_script_folder}"
+IMAGE_FOLDER="${image_folder}"
 IMAGEDATA_FILE="${imagedata_file}"
 DEBIAN_FRONTEND="noninteractive"
 INSTALLER_SCRIPT_FOLDER="${installer_script_folder}"
 DOCKERHUB_PULL_IMAGES="NO"
-
 PATCH_FILE="${PATCH_FILE:-runner-sdk8-${ARCH}.patch}"
