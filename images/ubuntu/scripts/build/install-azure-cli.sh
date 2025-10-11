f37f729fa2e50680fc0768cdd5b2c68b9c727fbc
rahulssv-ibm <rahul.vishwakarma2@ibm.com>
2025-10-11 21:34:38 +0530

    test azure cli

--- images/ubuntu/scripts/build/install-azure-cli.sh
+++ images/ubuntu/scripts/build/install-azure-cli.sh
@@ -4,13 +4,13 @@
 ##  Desc:  Install Azure CLI (az)
 ################################################################################
 
-if [[ "$ARCH" == "ppc64le" ]]; then 
-    # Placeholder for ppc64le-specific logic
-    echo "No actions defined for ppc64le architecture."
-elif [[ "$ARCH" == "s390x" ]]; then
-    # Placeholder for s390x-specific logic
-    echo "No actions defined for s390x architecture."
-else
+# if [[ "$ARCH" == "ppc64le" ]]; then 
+#     # Placeholder for ppc64le-specific logic
+#     echo "No actions defined for ppc64le architecture."
+# elif [[ "$ARCH" == "s390x" ]]; then
+#     # Placeholder for s390x-specific logic
+#     echo "No actions defined for s390x architecture."
+# else
     # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
     curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
@@ -18,4 +18,4 @@ else
 
     rm -f /etc/apt/sources.list.d/azure-cli.list
     rm -f /etc/apt/sources.list.d/azure-cli.list.save
-fi
+# fi
